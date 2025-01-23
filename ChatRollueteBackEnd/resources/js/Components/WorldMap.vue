523dd76081d2bd474c1064c0119e4a658a47527b
Max Hertel <maxherteljr@gmail.com>
2025-01-23 19:22:27 -0300

    home ok

--- ChatRollueteBackEnd/resources/js/Components/WorldMap.vue
+++ ChatRollueteBackEnd/resources/js/Components/WorldMap.vue
@@ -1,75 +1,135 @@
 <template>
-    <div class="container">
-      <div class="sidebar">
-        <button @click="highlightContinent('America')">América</button>
-        <button @click="highlightContinent('Africa')">África</button>
-        <button @click="highlightContinent('Asia')">Ásia</button>
-        <button @click="highlightContinent('Europa')">Europa</button>
-        <button @click="highlightContinent('Oceania')">Oceania</button>
+
+  <div class="flex flex-row ">
+    <div class="basis-1/4">
+      <div class="flex flex-col-reverse ">
+        <div> 
+          <button
+           @click="highlightContinent('Africa')"
+            class="mt-3 w-full rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2"
+            type="button">
+            África
+          </button>
+        </div>
+        <div> 
+          <button @click="highlightContinent('America')"
+            class="mt-3 w-full rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2"
+            type="button">
+            America
+          </button>
+        </div>
+        <div> 
+          <button  @click="highlightContinent('Asia')"class=" mt-3 w-full rounded-md bg-slate-800 py-2 px-4 border border-slate-800 text-center text-sm text-white 
+         shadow-md hover:shadow-lg focus:bg-slate-700 focus:border-slate-700 focus:shadow-none 
+         active:bg-slate-700 active:border-slate-700 active:shadow-none 
+          ml-2" type="button">
+          Ásia
+          </button>
+        </div>
+        <div> 
+          <button  @click="highlightContinent('Europe')"class="mt-3 w-full rounded-md bg-slate-800 py-2 px-4 border border-slate-800 text-center text-sm text-white 
+         shadow-md hover:shadow-lg focus:bg-slate-700 focus:border-slate-700 focus:shadow-none 
+         active:bg-slate-700 active:border-slate-700 active:shadow-none 
+          ml-2" type="button">
+          Europa
+          </button>
+        </div>
+        <div> 
+          <button  @click="highlightContinent('Oceania')"class="mt-3 w-full rounded-md bg-slate-800 py-2 px-4 border border-slate-800 text-center text-sm text-white 
+         shadow-md hover:shadow-lg focus:bg-slate-700 focus:border-slate-700 focus:shadow-none 
+         active:bg-slate-700 active:border-slate-700 active:shadow-none 
+          ml-2" type="button">
+          Oceania
+          </button>
+        </div>
       </div>
-      <div id="map" class="map"></div>
     </div>
-  </template>
-  
-  <script>
-  import L from 'leaflet';
-  // Importe os arquivos GeoJSON (ajuste os caminhos se necessário)
-  import americaGeoJSON from '../../maps/america.json'; 
-  import africaGeoJSON from '../../maps/africa.json';
-  // ... importe os outros GeoJSONs ...
-  
-  export default {
-    mounted() {
-      this.initMap();
+    <div class="basis-3/4 " >
+      <div id="map" style="min-height: 600px;" ></div>
+    </div>
+  </div>
+</template>
+<script>
+import L from 'leaflet';
+import americaGeoJSON from '../../maps/america.json';
+import africaGeoJSON from '../../maps/africa.json';
+import asiaGeoJSON from '../../maps/asia.json';
+import europeGeoJSON from '../../maps/europe.geo.json';
+import oceaniaGeoJSON from '../../maps/oceania.geo.json';
+
+export default {
+  mounted() {
+    this.initMap();
+  },
+  methods: {
+    initMap() {
+      this.map = L.map('map', {
+        dragging: false, // Desabilita o arrasto
+        zoomControl: false, // Remove o controle de zoom
+        scrollWheelZoom: false, // Desabilita o zoom com a roda do mouse
+        doubleClickZoom: false, // Desabilita o zoom com duplo clique
+        touchZoom: false, // Desabilita o zoom com toque em dispositivos touch
+      }).setView([0, 0], 2);
+
+      // Define o tamanho máximo do mapa
+      this.map.setMaxBounds([
+    [-90, -180], // Canto inferior esquerdo
+    [90, 180]   // Canto superior direito
+  ]);
+
+      this.americaLayer = L.geoJSON(americaGeoJSON, {
+        style: {   fillColor: 'green', weight: 0, fillOpacity: 1  }
+      }).addTo(this.map);
+
+      this.africaLayer = L.geoJSON(africaGeoJSON, {
+        style: { fillColor: 'green', weight: 0, fillOpacity: 1}
+      }).addTo(this.map);
+
+      this.asiaLayer = L.geoJSON(asiaGeoJSON, {
+        style: { fillColor: 'green', weight: 0, fillOpacity: 1}
+      }).addTo(this.map);
+
+      this.europeLayer = L.geoJSON(europeGeoJSON, {
+        style: { fillColor: 'green', weight: 0, fillOpacity: 1}
+      }).addTo(this.map);
+
+      this.oceaniaLayer = L.geoJSON(oceaniaGeoJSON, {
+        style: { fillColor: 'green', weight: 0, fillOpacity: 1}
+      }).addTo(this.map);
+
+       // Ajusta o zoom para que todos os dados caibam no mapa
+  // this.map.fitBounds(
+  //   L.geoJSON(americaGeoJSON).getBounds()
+  //     .extend(L.geoJSON(africaGeoJSON).getBounds())
+  //     .extend(L.geoJSON(asiaGeoJSON).getBounds())
+  //     .extend(L.geoJSON(europeGeoJSON).getBounds())
+  //     .extend(L.geoJSON(oceaniaGeoJSON).getBounds())
+  //     // ... estenda para outros continentes
+  // );
     },
-    methods: {
-      initMap() {
-        this.map = L.map('map', {
-    dragging: false, // Desabilita o arrasto
-    zoomControl: false, // Remove o controle de zoom
-    scrollWheelZoom: false, // Desabilita o zoom com a roda do mouse
-    doubleClickZoom: false, // Desabilita o zoom com duplo clique
-    touchZoom: false, // Desabilita o zoom com toque em dispositivos touch
-  }).setView([0, 0], 2);
-  
-        this.americaLayer = L.geoJSON(americaGeoJSON, {
-          style: { fillColor: 'red', weight: 1 }
-        }).addTo(this.map);
-  
-        this.africaLayer = L.geoJSON(africaGeoJSON, {
-          style: { fillColor: 'green', weight: 1 }
-        }).addTo(this.map);
-  
-        // ... adicione os outros continentes ...
-      },
-      highlightContinent(continent) { 
-        // Remova o destaque de todos os continentes (ajuste as cores)
-        this.americaLayer.setStyle({ fillColor: 'red' }); 
-        this.africaLayer.setStyle({ fillColor: 'green' });
-        // ... repita para os outros continentes ...
-  
-        // Destaque o continente selecionado
-        if (continent === 'America') {
-          this.americaLayer.setStyle({ fillColor: 'yellow' });
-        } else if (continent === 'Africa') {
-          this.africaLayer.setStyle({ fillColor: 'yellow' });
-        }
-        // ... repita para os outros continentes ...
+    highlightContinent(continent) {
+      // Remova o destaque de todos os continentes (ajuste as cores)
+      this.americaLayer.setStyle({ fillColor: 'green' });
+      this.africaLayer.setStyle({ fillColor: 'green' });
+      this.asiaLayer.setStyle({ fillColor: 'green' });
+      this.europeLayer.setStyle({ fillColor: 'green' });
+      this.oceaniaLayer.setStyle({ fillColor: 'green' });
+      // ... repita para os outros continentes ...
+
+      // Destaque o continente selecionado
+      if (continent === 'America') {
+        this.americaLayer.setStyle({ fillColor: 'white' });
+      } else if (continent === 'Africa') {
+        this.africaLayer.setStyle({ fillColor: 'white' });
+      } else if (continent === 'Asia') {
+        this.asiaLayer.setStyle({ fillColor: 'white' });
+      } else if (continent === 'Europe') {
+        this.europeLayer.setStyle({ fillColor: 'white' });
+      } else if (continent === 'Oceania') {
+        this.oceaniaLayer.setStyle({ fillColor: 'white' });
       }
+      // ... repita para os outros continentes ...
     }
-  };
-  </script>
-<style>
-.container {
-  display: flex;
-}
-
-.sidebar {
-  width: 200px;
-}
-
-.map {
-  flex: 1;
-  height: 50    0px; 
-}
-</style>
+  }
+};
+</script>
--- ChatRollueteBackEnd/resources/js/Pages/Welcome.vue
+++ ChatRollueteBackEnd/resources/js/Pages/Welcome.vue
@@ -18,42 +18,17 @@ defineProps({
     },
 });
 
-function handleImageError() {
-    document.getElementById('screenshot-container')?.classList.add('!hidden');
-    document.getElementById('docs-card')?.classList.add('!row-span-1');
-    document.getElementById('docs-card-content')?.classList.add('!flex-row');
-    document.getElementById('background')?.classList.add('!hidden');
-}
+
 </script>
-<style>
-.map {
-    flex: 1;
-    height: 400px;
-    /* Altura fixa */
-    /* ou */
-    max-height: 400px;
-    /* Altura máxima */
-}
 
-.container {
-    display: flex;
-    max-width: 800px;
-    /* Largura máxima */
-}
-</style>
+<template class="h-14 bg-linear-65 from-purple-500 to-pink-500">
 
-<template>
-
-    <Head title="Welcome" />
-    <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
-        <img id="background" class="absolute -left-20 top-0 max-w-[877px]"
-            src="https://laravel.com/assets/img/welcome/background.svg" />
-        <div
-            class="relative flex min-h-screen flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
+    <div class=" text-black dark:bg-black dark:text-white ">
+        <div class="relative flex min-h-screen flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white ">
             <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
-                <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
-                    <div class="flex lg:col-start-2 lg:justify-center">
-
+                <header class="grid grid-cols-2 gap-2 py-10 lg:grid-cols-3" style="margin-bottom: 50px;">
+                    <div class="flex lg:col-start-2 justify-left">
+                        Chat Roulette for Business
                     </div>
                     <nav v-if="canLogin" class="-mx-3 flex flex-1 justify-end">
                         <Link v-if="$page.props.auth.user" :href="route('dashboard')"
@@ -72,16 +47,9 @@ function handleImageError() {
                     </nav>
                 </header>
 
-                <div class="relative flex flex-col selection:bg-[#FF2D20] selection:text-white">
-                    <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
-                        {/* ... */}
-                        <main class="mt-6 flex flex-col">
-                            <WorldMap class="flex-grow" />
-                        </main>
-                        {/* ... */}
-                    </div>
-                </div>
+                <WorldMap/>
                 <footer class="py-16 text-center text-sm text-black dark:text-white/70">
+                    Links
                 </footer>
             </div>
         </div>
